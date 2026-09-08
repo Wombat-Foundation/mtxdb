5214f498be2971ba160c1f58d19434a2112d32be
Shane Jaroch <chown_tee@proton.me>
2026-09-07 20:12:12 -0400

    chore: fix version emission in CLI

---
+++ mtxdb-cli/build.rs
@@ -0,0 +1,9 @@
+fn main() {
+    let git_describe = std::process::Command::new("git")
+        .args(["describe", "--always", "--dirty"])
+        .current_dir(env!("CARGO_MANIFEST_DIR"))
+        .output()
+        .ok()
+        .filter(|o| o.status.success()).map_or_else(|| env!("CARGO_PKG_VERSION").to_owned(), |o| String::from_utf8_lossy(&o.stdout).trim().to_owned());
+    println!("cargo:rustc-env=GIT_DESCRIBE={git_describe}");
+}
